b574796d8755c84c04ed996855626683ce2a92d9
CaramelToffee <r.rmarsu@gmail.com>
2025-05-23 23:00:40 +0300

    feat: appointments model&sql

---
+++ backend/migrations/20250523195820_appointments.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE appointments IF NOT EXISTS (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    master_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    scheduled_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    status VARCHAR(50) DEFAULT 'pending'
+);
+
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE appointments IF EXISTS;
+-- +goose StatementEnd
