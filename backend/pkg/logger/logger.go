20fd4363182d8b834854fbab26c0c1deb64c9c58
CaramelToffee <r.rmarsu@gmail.com>
2025-05-23 23:02:51 +0300

    feat: logger

---
+++ backend/pkg/logger/logger.go
@@ -0,0 +1,35 @@
+package logger
+
+import (
+	"context"
+
+	"go.uber.org/zap"
+)
+
+type ctxKey struct{}
+
+var defaultLogger *zap.Logger
+
+func init() {
+	var err error
+	defaultLogger, err = zap.NewProduction()
+	if err != nil {
+		panic("failed to create default zap logger: " + err.Error())
+	}
+}
+
+func WithLogger(ctx context.Context, logger *zap.Logger) context.Context {
+	return context.WithValue(ctx, ctxKey{}, logger)
+}
+
+func FromContext(ctx context.Context) *zap.Logger {
+	logger, ok := ctx.Value(ctxKey{}).(*zap.Logger)
+	if !ok || logger == nil {
+		return defaultLogger
+	}
+	return logger
+}
+
+func SugarFromContext(ctx context.Context) *zap.SugaredLogger {
+	return FromContext(ctx).Sugar()
+}
