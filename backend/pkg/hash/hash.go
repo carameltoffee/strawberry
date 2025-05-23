3277517900c23ee58b484384d11f16092ba27edb
CaramelToffee <r.rmarsu@gmail.com>
2025-05-23 22:42:27 +0300

    feat: hasher

---
+++ backend/pkg/hash/hash.go
@@ -0,0 +1,31 @@
+package hasher
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"hash"
+	"sync"
+)
+
+type Hasher struct {
+	pool sync.Pool
+}
+
+func NewHasher() *Hasher {
+	return &Hasher{
+		pool: sync.Pool{
+			New: func() interface{} {
+				return sha256.New()
+			},
+		},
+	}
+}
+
+func (h *Hasher) HashString(s string) string {
+	hasher := h.pool.Get().(hash.Hash)
+	hasher.Reset()
+	hasher.Write([]byte(s))
+	sum := hasher.Sum(nil)
+	h.pool.Put(hasher)
+	return hex.EncodeToString(sum)
+}
