875bd826e279fb199292d4007efa4dd12dcbde38
CaramelToffee <r.rmarsu@gmail.com>
2025-05-25 23:57:07 +0300

    fix: appointments service

--- backend/internal/repository/repository.go
+++ backend/internal/repository/repository.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"context"
 	"strawberry/internal/models"
+	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -14,14 +15,20 @@ type Repository struct {
 
 type Users interface {
 	Create(ctx context.Context, us *models.User) (int64, error)
-	Update(ctx context.Context, us *models.User) (error)
-	Delete(ctx context.Context, id int64) (error)
+	Update(ctx context.Context, us *models.User) error
+	Delete(ctx context.Context, id int64) error
+	GetByFullName(ctx context.Context, fn string) ([]models.User, error)
 	GetByUsername(ctx context.Context, un string) (*models.User, error)
 	GetMastersByRating(ctx context.Context) ([]models.User, error)
-	GetMastersBySpecialization(ctx context.Context, s string) ([]models.User,error) 
+	GetMastersBySpecialization(ctx context.Context, s string) ([]models.User, error)
 }
 
 type Appointments interface {
+	Create(ctx context.Context,a *models.Appointment) (int64, error)
+	Delete(ctx context.Context,id int64) (error)
+	GetByUserId(ctx context.Context,id int64) ([]models.Appointment,error)
+	GetByMasterId(ctx context.Context,id int64) ([]models.Appointment,error)
+	GetByDate(ctx context.Context, id int64, date time.Time) ([]models.Appointment,error)
 }
 
 func New(db *pgxpool.Pool) *Repository {
