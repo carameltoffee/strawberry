18c0e536c37ab6afba647f8b0b7483c501d303a6
CaramelToffee <r.rmarsu@gmail.com>
2025-05-26 19:17:38 +0300

    fix: make scheduled_at unique

--- backend/migrations/20250523195820_appointments.sql
+++ backend/migrations/20250523195820_appointments.sql
@@ -4,7 +4,7 @@ CREATE TABLE appointments IF NOT EXISTS (
     id SERIAL PRIMARY KEY,
     user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     master_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    scheduled_at TIMESTAMP NOT NULL,
+    scheduled_at TIMESTAMP NOT NULL UNIQUE,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     status VARCHAR(50) DEFAULT 'pending'
 );
