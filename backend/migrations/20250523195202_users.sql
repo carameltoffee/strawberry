bfab4a3ce22141c695e97ae7ab1602b1f2f44a0e
CaramelToffee <r.rmarsu@gmail.com>
2025-05-27 23:39:39 +0300

    feat: added password

--- backend/migrations/20250523195202_users.sql
+++ backend/migrations/20250523195202_users.sql
@@ -4,6 +4,7 @@ CREATE TABLE users IF NOT EXISTS (
     id SERIAL PRIMARY KEY,
     full_name VARCHAR(255) NOT NULL,
     username VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
     registered_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     average_rating FLOAT CHECK (average_rating >= 0 AND average_rating <= 5),
     specialization VARCHAR(100) DEFAULT 'user'
