69afe974cc35becc42f9596ae60f544aa20ae78f
CaramelToffee <r.rmarsu@gmail.com>
2025-05-28 22:52:53 +0300

    feat: add config

---
+++ backend/internal/config/config.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"time"
+
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	App struct {
+		Port int `envconfig:"REST_PORT" default:"8080"`
+	}
+	Database struct {
+		User     string `envconfig:"DB_USER" required:"true"`
+		Password string `envconfig:"DB_PASSWORD" required:"true"`
+		Host     string `envconfig:"DB_HOST" default:"localhost"`
+		Port     string `envconfig:"DB_PORT" default:"5432"`
+		Name     string `envconfig:"DB_NAME" required:"true"`
+		SSLMode  string `envconfig:"DB_SSLMODE" default:"disable"`
+	}
+	Jwt struct {
+		SecretKey string        `envconfig:"SECRET_KEY" required:"true"`
+		TTL       time.Duration `envconfig:"TTL" required:"true"`
+	}
+}
+
+func MustLoad() Config {
+	var cfg Config
+	if err := envconfig.Process("", &cfg); err != nil {
+		panic("cannot load config!!")
+	}
+	return cfg
+}
