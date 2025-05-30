f34106ac66ee0668b83524b0dc6e1844d9908882
CaramelToffee <r.rmarsu@gmail.com>
2025-05-31 00:36:58 +0300

    fix: schedules

--- backend/migrations/20250530211511_schedules.sql
+++ backend/migrations/20250530211511_schedules.sql
@@ -6,11 +6,9 @@ CREATE TABLE IF NOT EXISTS schedules (
     day_of_week VARCHAR(10) NOT NULL CHECK (day_of_week IN ('monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday')),
     time_start TIME NOT NULL,
     time_end TIME NOT NULL,
-    is_day_off BOOLEAN NOT NULL DEFAULT FALSE
+    is_day_off BOOLEAN NOT NULL DEFAULT FALSE,
+    CONSTRAINT schedules_user_day_unique UNIQUE (user_id, day_of_week)
 );
-
-CREATE UNIQUE INDEX IF NOT EXISTS idx_unique_schedule_per_user_day
-ON schedules (user_id, LOWER(day_of_week));
 -- +goose StatementEnd
 
 -- +goose Down
