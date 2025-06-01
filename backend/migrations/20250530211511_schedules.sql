9a8a113acb090c851e2a40d48371461a663ec520
CaramelToffee <r.rmarsu@gmail.com>
2025-06-01 22:54:36 +0300

    refactor: schedule def

--- backend/migrations/20250530211511_schedules.sql
+++ backend/migrations/20250530211511_schedules.sql
@@ -1,17 +1,21 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE IF NOT EXISTS schedules (
+CREATE TABLE IF NOT EXISTS schedule_slots (
     id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     day_of_week VARCHAR(10) NOT NULL CHECK (day_of_week IN ('monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday')),
-    time_start TIME NOT NULL,
-    time_end TIME NOT NULL,
-    is_day_off BOOLEAN NOT NULL DEFAULT FALSE,
-    CONSTRAINT schedules_user_day_unique UNIQUE (user_id, day_of_week)
+    slot TIME NOT NULL,
+    UNIQUE (user_id, day_of_week, slot)
+);
+CREATE TABLE IF NOT EXISTS days_off_dates (
+    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    date DATE NOT NULL,
+    PRIMARY KEY (user_id, date)
 );
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DROP TABLE IF EXISTS schedules;
+DROP TABLE IF EXISTS schedule_slots;
+DROP TABLE IF EXISTS days_off_dates;
 -- +goose StatementEnd
