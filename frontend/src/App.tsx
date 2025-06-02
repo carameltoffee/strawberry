62ec8689e6a7797e89da46ea1d8a730c93e544bb
CaramelToffee <r.rmarsu@gmail.com>
2025-06-02 23:51:26 +0300

    feat: app route auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Layout from "./components/layout/layout";
 import Welcome from "./components/welcome/welcome";
 import Reservation from "./components/reservation/reservation";
 import SchedulePage from "./components/schedule/schedule";
+import AuthForm from "./components/auth/auth";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" Component={Welcome} />
           <Route path="/reservation/:id" Component={Reservation} />
           <Route path="/user" Component={SchedulePage} />
+          <Route path="/auth" Component={AuthForm}/>
         </Routes>
       </Layout>
     </Router>
