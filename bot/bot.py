498dec498f99aa4cc1db76288e440a2c8b449f6f
CaramelToffee <r.rmarsu@gmail.com>
2025-06-03 23:04:53 +0300

    feat: rabbitmq

--- bot/bot.py
+++ bot/bot.py
@@ -4,6 +4,8 @@ from handlers.start import create_start_router
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 from api.api_client import StrawberryAPIClient
+from utils.utils import JWTDecoder
+from utils.notifications import run_bot_consumer
 
 load_dotenv()
 
@@ -16,6 +18,8 @@ async def main():
 
     api_client = StrawberryAPIClient(API_BASE_URL)
     await api_client.start()
+    jwt_decoder = JWTDecoder()
+    jwt_decoder.set_secret(os.getenv("JWT_SECRET"))
 
     token = os.getenv("BOT_TOKEN")
     if not token:
@@ -23,11 +27,14 @@ async def main():
 
     bot = Bot(token=token)
     dp = Dispatcher()
+    
+    run_bot_consumer(bot, os.getenv("RABBITMQ_URL"), os.getenv("EXCHANGE_NAME"), 
+                    routing_keys=["appointments.created", "appointments.deleted"])
 
     days_off_router = create_days_off_router(api_client)
-    schedule_router = create_schedule_router(api_client)
+    schedule_router = create_schedule_router(api_client, jwt_decoder)
     work_hours_router = create_work_hours_router(api_client)
-    start_router = create_start_router(api_client)
+    start_router = create_start_router(api_client, jwt_decoder)
 
     dp.include_router(days_off_router)
     dp.include_router(schedule_router)
