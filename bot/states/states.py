a5c09ae899609391d02f89c968cf358b3584c902
CaramelToffee <r.rmarsu@gmail.com>
2025-06-04 23:17:48 +0300

    feat: new states by date

--- bot/states/states.py
+++ bot/states/states.py
@@ -4,3 +4,5 @@ class States(StatesGroup):
     choose_weekend_days = State()
     remove_weekend_days = State()
     set_work_hours = State()
+    set_work_hours_by_date = State()
+    delete_work_hours_by_date = State()
