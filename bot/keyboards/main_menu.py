49ba046aff3ad80ca1f6a2049ab81aa2234004be
CaramelToffee <r.rmarsu@gmail.com>
2025-06-04 23:17:24 +0300

    feat: new buttons to api

--- bot/keyboards/main_menu.py
+++ bot/keyboards/main_menu.py
@@ -4,7 +4,9 @@ main_menu = ReplyKeyboardMarkup(
     keyboard=[
         [KeyboardButton(text="Добавить выходной")],
         [KeyboardButton(text="Удалить выходной")],
-        [KeyboardButton(text="Установить часы приёма")],
+        [KeyboardButton(text="Установить часы приёма на день недели")],
+        [KeyboardButton(text="Установить часы приёма на дату")],
+        [KeyboardButton(text="Удалить часы приёма на дату")],
         [KeyboardButton(text="Показать расписание")],
     ],
     resize_keyboard=True,
