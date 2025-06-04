e1762dc1352e257e70bf1088f969f81e47598381
CaramelToffee <r.rmarsu@gmail.com>
2025-06-04 23:14:06 +0300

    feat: new table date_slots

--- backend/migrations/20250530211514_schedules.sql
+++ backend/migrations/20250530211514_schedules.sql
@@ -7,6 +7,14 @@ CREATE TABLE IF NOT EXISTS schedule_slots (
     slot TIME NOT NULL,
     UNIQUE (user_id, day_of_week, slot)
 );
+
+CREATE TABLE IF NOT EXISTS date_slots (
+    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    date DATE NOT NULL, 
+    slot TIME NOT NULL,
+    PRIMARY KEY (user_id, date, slot)
+);
+
 CREATE TABLE IF NOT EXISTS days_off_dates (
     user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     date DATE NOT NULL,
@@ -18,4 +26,5 @@ CREATE TABLE IF NOT EXISTS days_off_dates (
 -- +goose StatementBegin
 DROP TABLE IF EXISTS schedule_slots;
 DROP TABLE IF EXISTS days_off_dates;
+DROP TABLE IF EXISTS date_slots;
 -- +goose StatementEnd
