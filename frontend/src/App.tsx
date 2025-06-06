41eb9c54f9291aec12103c8d8518b554b3338c31
CaramelToffee <r.rmarsu@gmail.com>
2025-06-06 23:11:16 +0300

    fix: dependecies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,46 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/layout";
-import Welcome from "./components/welcome/welcome";
-import Reservation from "./components/reservation/reservation";
-import UserPage from "./pages/profile_page";
-import AuthForm from "./components/auth/auth";
+import Reservation from "./pages/reservation/reservation";
+import UserPage from "./pages/profile/profile_page";
+import AuthPage from "./pages/auth/auth";
+import { useState, useEffect } from "react";
+import { Main } from "./pages/main/main";
 
 function App() {
-  return (
-    <Router>
-      <Layout title={__APP_NAME__}>
-        <Routes>
-          <Route path="/" Component={Welcome} />
-          <Route path="/reservation/:id" Component={Reservation} />
-          <Route path="/user" Component={UserPage} />
-          <Route path="/auth" Component={AuthForm}/>
-        </Routes>
-      </Layout>
-    </Router>
-  );
-}
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
 
+	useEffect(() => {
+		const token = localStorage.getItem("token");
+		setIsAuthenticated(!!token);
+	}, []);
+
+	const handleLogin = (token: string) => {
+		localStorage.setItem("token", token);
+		setIsAuthenticated(true);
+	};
+
+	const handleLogout = () => {
+		localStorage.removeItem("token");
+		setIsAuthenticated(false);
+	};
+
+	return (
+		<Router>
+			<Layout
+				title={__APP_NAME__}
+				isAuthenticated={isAuthenticated}
+				onLogout={handleLogout}
+			>
+				<Routes>
+					<Route path="/" Component={Main} />
+					<Route path="/reservation/:id" Component={Reservation} />
+					<Route path="/user" Component={UserPage} />
+					<Route path="/auth/login" element={<AuthPage onLogin={handleLogin}/>} />
+					<Route path="/auth/register" element={<AuthPage />} />
+				</Routes>
+			</Layout>
+		</Router>
+	);
+}
 
 export default App;
