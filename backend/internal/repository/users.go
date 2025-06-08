d5e09c5874cc4851ec50894ec1bafb67d1ea07df
CaramelToffee <r.rmarsu@gmail.com>
2025-06-08 23:57:08 +0300

    feat: email field

--- backend/internal/repository/users.go
+++ backend/internal/repository/users.go
@@ -22,13 +22,13 @@ func newPostgresUsersRepository(db *pgxpool.Pool) Users {
 
 func (r *postgresUsersRepository) Create(ctx context.Context, us *models.User) (int64, error) {
 	query := `
-		INSERT INTO users (full_name, username, password, average_rating, specialization)
-		VALUES ($1, $2, $3, $4, $5)
+		INSERT INTO users (full_name, username, password, email, average_rating, specialization)
+		VALUES ($1, $2, $3, $4, $5, $6)
 		RETURNING id;
 	`
 	var id int64
 	err := r.db.QueryRow(ctx, query,
-		us.FullName, us.Username, us.Password, us.AverageRating, us.Specialization).Scan(&id)
+		us.FullName, us.Username, us.Password, us.Email, us.AverageRating, us.Specialization).Scan(&id)
 	if err != nil {
 		var pgErr *pgconn.PgError
 		if errors.As(err, &pgErr) && pgErr.Code == pgerrcode.UniqueViolation {
@@ -42,11 +42,11 @@ func (r *postgresUsersRepository) Create(ctx context.Context, us *models.User) (
 func (r *postgresUsersRepository) Update(ctx context.Context, us *models.User) error {
 	query := `
 		UPDATE users 
-		SET full_name = $1, username = $2, password = $3, average_rating = $4, specialization = $5
-		WHERE id = $6;
+		SET full_name = $1, username = $2, password = $3, email = $4, average_rating = $5, specialization = $6
+		WHERE id = $7;
 	`
 	cmdTag, err := r.db.Exec(ctx, query,
-		us.FullName, us.Username, us.Password, us.AverageRating, us.Specialization, us.Id)
+		us.FullName, us.Username, us.Password, us.Email, us.AverageRating, us.Specialization, us.Id)
 	if err != nil {
 		return err
 	}
@@ -69,7 +69,7 @@ func (r *postgresUsersRepository) Delete(ctx context.Context, id int64) error {
 
 func (r *postgresUsersRepository) GetByFullName(ctx context.Context, fn string) ([]models.User, error) {
 	rows, err := r.db.Query(ctx, `
-		SELECT id, full_name, username, password, registered_at, average_rating, specialization
+		SELECT id, full_name, username, password, email, registered_at, average_rating, specialization
 		FROM users WHERE full_name = $1;
 	`, fn)
 	if err != nil {
@@ -80,7 +80,7 @@ func (r *postgresUsersRepository) GetByFullName(ctx context.Context, fn string) 
 	var users []models.User
 	for rows.Next() {
 		var u models.User
-		if err := rows.Scan(&u.Id, &u.FullName, &u.Username, &u.Password, &u.RegisteredAt, &u.AverageRating, &u.Specialization); err != nil {
+		if err := rows.Scan(&u.Id, &u.FullName, &u.Username, &u.Password, &u.Email, &u.RegisteredAt, &u.AverageRating, &u.Specialization); err != nil {
 			return nil, err
 		}
 		users = append(users, u)
@@ -93,12 +93,12 @@ func (r *postgresUsersRepository) GetByFullName(ctx context.Context, fn string) 
 
 func (r *postgresUsersRepository) GetByUsername(ctx context.Context, un string) (*models.User, error) {
 	row := r.db.QueryRow(ctx, `
-		SELECT id, full_name, username, password, registered_at, average_rating, specialization
+		SELECT id, full_name, username, password, email, registered_at, average_rating, specialization
 		FROM users WHERE username = $1;
 	`, un)
 
 	var u models.User
-	err := row.Scan(&u.Id, &u.FullName, &u.Username, &u.Password, &u.RegisteredAt, &u.AverageRating, &u.Specialization)
+	err := row.Scan(&u.Id, &u.FullName, &u.Username, &u.Password, &u.Email, &u.RegisteredAt, &u.AverageRating, &u.Specialization)
 	if errors.Is(err, pgx.ErrNoRows) {
 		return nil, ErrNoUsers
 	}
@@ -110,7 +110,7 @@ func (r *postgresUsersRepository) GetByUsername(ctx context.Context, un string) 
 
 func (r *postgresUsersRepository) GetMastersByRating(ctx context.Context) ([]models.User, error) {
 	rows, err := r.db.Query(ctx, `
-		SELECT id, full_name, username, password, registered_at, average_rating, specialization
+		SELECT id, full_name, username, password, email, registered_at, average_rating, specialization
 		FROM users WHERE specialization != 'user'
 		ORDER BY average_rating DESC;
 	`)
@@ -122,7 +122,7 @@ func (r *postgresUsersRepository) GetMastersByRating(ctx context.Context) ([]mod
 	var users []models.User
 	for rows.Next() {
 		var u models.User
-		if err := rows.Scan(&u.Id, &u.FullName, &u.Username, &u.Password, &u.RegisteredAt, &u.AverageRating, &u.Specialization); err != nil {
+		if err := rows.Scan(&u.Id, &u.FullName, &u.Username, &u.Password, &u.Email, &u.RegisteredAt, &u.AverageRating, &u.Specialization); err != nil {
 			return nil, err
 		}
 		users = append(users, u)
@@ -135,7 +135,7 @@ func (r *postgresUsersRepository) GetMastersByRating(ctx context.Context) ([]mod
 
 func (r *postgresUsersRepository) GetMastersBySpecialization(ctx context.Context, s string) ([]models.User, error) {
 	rows, err := r.db.Query(ctx, `
-		SELECT id, full_name, username, password, registered_at, average_rating, specialization
+		SELECT id, full_name, username, password, email, registered_at, average_rating, specialization
 		FROM users WHERE specialization = $1;
 	`, s)
 	if err != nil {
@@ -146,7 +146,7 @@ func (r *postgresUsersRepository) GetMastersBySpecialization(ctx context.Context
 	var users []models.User
 	for rows.Next() {
 		var u models.User
-		if err := rows.Scan(&u.Id, &u.FullName, &u.Username, &u.Password, &u.RegisteredAt, &u.AverageRating, &u.Specialization); err != nil {
+		if err := rows.Scan(&u.Id, &u.FullName, &u.Username, &u.Password, &u.Email, &u.RegisteredAt, &u.AverageRating, &u.Specialization); err != nil {
 			return nil, err
 		}
 		users = append(users, u)
@@ -159,12 +159,12 @@ func (r *postgresUsersRepository) GetMastersBySpecialization(ctx context.Context
 
 func (r *postgresUsersRepository) GetById(ctx context.Context, id int64) (*models.User, error) {
 	row := r.db.QueryRow(ctx, `
-		SELECT id, full_name, username, registered_at, average_rating, specialization
+		SELECT id, full_name, username, email, registered_at, average_rating, specialization
 		FROM users WHERE id = $1;
 	`, id)
 
 	var u models.User
-	err := row.Scan(&u.Id, &u.FullName, &u.Username, &u.RegisteredAt, &u.AverageRating, &u.Specialization)
+	err := row.Scan(&u.Id, &u.FullName, &u.Username, &u.Email, &u.RegisteredAt, &u.AverageRating, &u.Specialization)
 	if errors.Is(err, pgx.ErrNoRows) {
 		return nil, ErrNoUsers
 	}
