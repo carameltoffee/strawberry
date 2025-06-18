0b0b23e523834732f9aeb4e3c117f31abad2ebf3
CaramelToffee <r.rmarsu@gmail.com>
2025-06-18 23:49:52 +0300

    fix: return null user if {}

--- frontend2/src/components/Auth/Auth.reducers.ts
+++ frontend2/src/components/Auth/Auth.reducers.ts
@@ -16,36 +16,43 @@ const initialState: AuthState = {
 
 export function authReducer(state = initialState, action: AuthActionTypes): AuthState {
      switch (action.type) {
-     case REQUEST:
-          return {...state, loading:true, error:null};
-     case LOGIN_SUCCESS:
-          localStorage.setItem('token', JSON.stringify(action.payload.token))
-          localStorage.setItem('user', JSON.stringify(action.payload.user))
-          return {
-               user: action.payload.user,
-               token: action.payload.token,
-               loading: false,
-               error: null,
-          }
-     case LOGIN_FAILURE:
-          return {...state, loading:false, error:action.payload};
-     case REGISTER_SUCCESS:
-          return {...state, loading:false, error:null};
-     case REGISTER_FAILURE:
-          return {...state, loading:false, error:action.payload};
-     case SEND_CODE_SUCCESS:
-          return {...state, loading:false, error:null};
-     case SEND_CODE_FAILURE:
-          return {...state, loading:false, error:action.payload};
-     case USER_LOADED:
-          return {loading:false, token:action.payload.token ,user: action.payload.user, error: null};
-     case LOGOUT:
-          localStorage.removeItem('user');
-          localStorage.removeItem('token');
-          return {...state, user:null, token:null, loading:false, error:null};
-     default:
-          return state;
+          case REQUEST:
+               return { ...state, loading: true, error: null };
+          case LOGIN_SUCCESS:
+               localStorage.setItem('token', JSON.stringify(action.payload.token))
+               localStorage.setItem('user', JSON.stringify(action.payload.user))
+               return {
+                    user: action.payload.user,
+                    token: action.payload.token,
+                    loading: false,
+                    error: null,
+               }
+          case LOGIN_FAILURE:
+               return { ...state, user: null, loading: false, error: action.payload };
+          case REGISTER_SUCCESS:
+               return { ...state, loading: false, error: null };
+          case REGISTER_FAILURE:
+               return { ...state, loading: false, error: action.payload };
+          case SEND_CODE_SUCCESS:
+               return { ...state, loading: false, error: null };
+          case SEND_CODE_FAILURE:
+               return { ...state, loading: false, error: action.payload };
+          case USER_LOADED:
+               if(isEmptyObject(action.payload.user)){
+                    return { loading: false, token: "", user: null, error: null };
+               }
+               return { loading: false, token: action.payload.token, user: action.payload.user, error: null };
+          case LOGOUT:
+               localStorage.removeItem('user');
+               localStorage.removeItem('token');
+               return { ...state, user: null, token: null, loading: false, error: null };
+          default:
+               return state;
      }
+}
+
+function isEmptyObject(obj: any): boolean {
+     return obj && typeof obj === 'object' && !Array.isArray(obj) && Object.keys(obj).length === 0;
 }
 
 export { AuthActionTypes };
