3c1bece7b828aacd33751681765b92739243a7ab
CaramelToffee <r.rmarsu@gmail.com>
2025-07-03 23:53:01 +0300

    feat: return bio & upd

--- backend/internal/repository/users.go
+++ backend/internal/repository/users.go
@@ -46,11 +46,11 @@ func (r *postgresUsersRepository) Create(ctx context.Context, us *models.User) (
 func (r *postgresUsersRepository) Update(ctx context.Context, us *models.User) error {
 	query := `
 		UPDATE users 
-		SET full_name = $1, username = $2, password = $3, email = $4, specialization = $5
+		SET full_name = $1, username = $2, bio = $3, email = $4, specialization = $5
 		WHERE id = $6;
 	`
 	cmdTag, err := r.db.Exec(ctx, query,
-		us.FullName, us.Username, us.Password, us.Email, us.Specialization, us.Id)
+		us.FullName, us.Username, us.Bio, us.Email, us.Specialization, us.Id)
 	if err != nil {
 		return err
 	}
@@ -70,10 +70,9 @@ func (r *postgresUsersRepository) Delete(ctx context.Context, id int64) error {
 	}
 	return nil
 }
-
 func (r *postgresUsersRepository) GetByFullName(ctx context.Context, fn string) ([]models.User, error) {
 	rows, err := r.db.Query(ctx, `
-		SELECT id, full_name, username, password, email, registered_at, specialization
+		SELECT id, full_name, username, password, email, registered_at, specialization, bio
 		FROM users WHERE full_name = $1;
 	`, fn)
 	if err != nil {
@@ -84,7 +83,7 @@ func (r *postgresUsersRepository) GetByFullName(ctx context.Context, fn string) 
 	var users []models.User
 	for rows.Next() {
 		var u models.User
-		if err := rows.Scan(&u.Id, &u.FullName, &u.Username, &u.Password, &u.Email, &u.RegisteredAt, &u.Specialization); err != nil {
+		if err := rows.Scan(&u.Id, &u.FullName, &u.Username, &u.Password, &u.Email, &u.RegisteredAt, &u.Specialization, &u.Bio); err != nil {
 			return nil, err
 		}
 		users = append(users, u)
@@ -97,12 +96,12 @@ func (r *postgresUsersRepository) GetByFullName(ctx context.Context, fn string) 
 
 func (r *postgresUsersRepository) GetByUsername(ctx context.Context, un string) (*models.User, error) {
 	row := r.db.QueryRow(ctx, `
-		SELECT id, full_name, username, password, email, registered_at, specialization
+		SELECT id, full_name, username, password, email, registered_at, specialization, bio
 		FROM users WHERE username = $1;
 	`, un)
 
 	var u models.User
-	err := row.Scan(&u.Id, &u.FullName, &u.Username, &u.Password, &u.Email, &u.RegisteredAt, &u.Specialization)
+	err := row.Scan(&u.Id, &u.FullName, &u.Username, &u.Password, &u.Email, &u.RegisteredAt, &u.Specialization, &u.Bio)
 	if errors.Is(err, pgx.ErrNoRows) {
 		return nil, ErrNoUsers
 	}
@@ -114,7 +113,7 @@ func (r *postgresUsersRepository) GetByUsername(ctx context.Context, un string) 
 
 func (r *postgresUsersRepository) GetMastersByRating(ctx context.Context) ([]models.User, error) {
 	rows, err := r.db.Query(ctx, `
-		SELECT id, full_name, username, password, email, registered_at, specialization
+		SELECT id, full_name, username, password, email, registered_at, specialization, bio
 		FROM users WHERE specialization != 'user';
 	`)
 	if err != nil {
@@ -125,7 +124,7 @@ func (r *postgresUsersRepository) GetMastersByRating(ctx context.Context) ([]mod
 	var users []models.User
 	for rows.Next() {
 		var u models.User
-		if err := rows.Scan(&u.Id, &u.FullName, &u.Username, &u.Password, &u.Email, &u.RegisteredAt, &u.Specialization); err != nil {
+		if err := rows.Scan(&u.Id, &u.FullName, &u.Username, &u.Password, &u.Email, &u.RegisteredAt, &u.Specialization, &u.Bio); err != nil {
 			return nil, err
 		}
 		users = append(users, u)
@@ -138,7 +137,7 @@ func (r *postgresUsersRepository) GetMastersByRating(ctx context.Context) ([]mod
 
 func (r *postgresUsersRepository) GetMastersBySpecialization(ctx context.Context, s string) ([]models.User, error) {
 	rows, err := r.db.Query(ctx, `
-		SELECT id, full_name, username, password, email, registered_at, specialization
+		SELECT id, full_name, username, password, email, registered_at, specialization, bio
 		FROM users WHERE specialization = $1;
 	`, s)
 	if err != nil {
@@ -149,7 +148,7 @@ func (r *postgresUsersRepository) GetMastersBySpecialization(ctx context.Context
 	var users []models.User
 	for rows.Next() {
 		var u models.User
-		if err := rows.Scan(&u.Id, &u.FullName, &u.Username, &u.Password, &u.Email, &u.RegisteredAt, &u.Specialization); err != nil {
+		if err := rows.Scan(&u.Id, &u.FullName, &u.Username, &u.Password, &u.Email, &u.RegisteredAt, &u.Specialization, &u.Bio); err != nil {
 			return nil, err
 		}
 		users = append(users, u)
@@ -162,12 +161,12 @@ func (r *postgresUsersRepository) GetMastersBySpecialization(ctx context.Context
 
 func (r *postgresUsersRepository) GetById(ctx context.Context, id int64) (*models.User, error) {
 	row := r.db.QueryRow(ctx, `
-		SELECT id, full_name, username, email, registered_at, specialization
+		SELECT id, full_name, username, email, registered_at, specialization, bio
 		FROM users WHERE id = $1;
 	`, id)
 
 	var u models.User
-	err := row.Scan(&u.Id, &u.FullName, &u.Username, &u.Email, &u.RegisteredAt, &u.Specialization)
+	err := row.Scan(&u.Id, &u.FullName, &u.Username, &u.Email, &u.RegisteredAt, &u.Specialization, &u.Bio)
 	if errors.Is(err, pgx.ErrNoRows) {
 		return nil, ErrNoUsers
 	}
@@ -179,7 +178,7 @@ func (r *postgresUsersRepository) GetById(ctx context.Context, id int64) (*model
 
 func (r *postgresUsersRepository) SearchUsers(ctx context.Context, query string) ([]models.User, error) {
 	sqlQuery := `
-        SELECT id, full_name, email, username, password, registered_at, specialization
+        SELECT id, full_name, email, username, password, registered_at, specialization, bio
         FROM users
         WHERE full_name ILIKE $1 OR username ILIKE $1 OR specialization ILIKE $1
     `
@@ -194,7 +193,7 @@ func (r *postgresUsersRepository) SearchUsers(ctx context.Context, query string)
 	var users []models.User
 	for rows.Next() {
 		var u models.User
-		err := rows.Scan(&u.Id, &u.FullName, &u.Email, &u.Username, &u.Password, &u.RegisteredAt, &u.Specialization)
+		err := rows.Scan(&u.Id, &u.FullName, &u.Email, &u.Username, &u.Password, &u.RegisteredAt, &u.Specialization, &u.Bio)
 		if err != nil {
 			return nil, err
 		}
@@ -209,7 +208,7 @@ func (r *postgresUsersRepository) SearchUsers(ctx context.Context, query string)
 
 func (r *postgresUsersRepository) GetByEmail(ctx context.Context, email string) (*models.User, error) {
 	const query = `
-        SELECT id, full_name, email, username, password, registered_at, specialization
+        SELECT id, full_name, email, username, password, registered_at, specialization, bio
         FROM users
         WHERE email = $1
         LIMIT 1;
@@ -224,6 +223,7 @@ func (r *postgresUsersRepository) GetByEmail(ctx context.Context, email string) 
 		&user.Password,
 		&user.RegisteredAt,
 		&user.Specialization,
+		&user.Bio,
 	)
 	if err != nil {
 		if err == pgx.ErrNoRows {
