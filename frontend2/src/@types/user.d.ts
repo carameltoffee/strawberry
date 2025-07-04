1ebbb0341e3c43a9a8467493e653e053f211804d
CaramelToffee <r.rmarsu@gmail.com>
2025-07-04 23:51:52 +0300

    feat: bio in iuser

--- frontend2/src/@types/user.d.ts
+++ frontend2/src/@types/user.d.ts
@@ -2,6 +2,7 @@ interface IUser {
      id: number;
      full_name: string;
      username: string;
+     bio: string;
      email: string;
      specialization?: string;
      registered_at: string;
